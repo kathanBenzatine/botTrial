33a43b6c8d1cbd3635338df0905cdbdfa12f214d
kalpesh-benzatine <kalpesh@benzatine.com>
2025-02-20 15:03:42 +0530

    asd

--- src/pages/Demo.jsx
+++ src/pages/Demo.jsx
@@ -1,11 +1,10 @@
 import { useEffect } from "react";
-import { initInvoice } from "@telegram-apps/sdk";
 import { useTelegram } from "@telegram-apps/sdk-react";
 export default function Demo() {
-  const invoice = initInvoice();
-  invoice.open("https://t.me/invoice/abIIks213", "url").then((status) => {
-    // Output: 'paid'
-    return console.log(status);
-  });
+  // const invoice = initInvoice();
+  // invoice.open("https://t.me/invoice/abIIks213", "url").then((status) => {
+  //   // Output: 'paid'
+  //   return console.log(status);
+  // });
   return <div>dewsdsdf ,m lk</div>;
 }
