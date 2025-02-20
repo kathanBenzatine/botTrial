45378060db79188eb98df4f9f31e9bb3de7b7bf5
kalpesh-benzatine <kalpesh@benzatine.com>
2025-02-20 15:39:30 +0530

    usrname

--- src/pages/Demo.jsx
+++ src/pages/Demo.jsx
@@ -3,20 +3,25 @@ import { useEffect } from "react";
 // import { useTelegram } from "@telegram-apps/sdk-react";
 export default function Demo() {
   const initiatePayment = () => {
-    try {
-      WebApp.openInvoice(
-        "https://t.me/$L-6lxqntuFUIAwAAw_E5X8dDOUE",
-        function (invoiceStatus) {
-          if (invoiceStatus.status === "paid") {
-            console.log("Payment successful!");
-          } else {
-            console.log("Payment failed or was cancelled.");
-          }
-        }
-      );
-    } catch (error) {
-      console.error("Error opening invoice:", error);
-    }
+    // try {
+    //   WebApp.openInvoice(
+    //     "https://t.me/$L-6lxqntuFUIAwAAw_E5X8dDOUE",
+    //     function (invoiceStatus) {
+    //       if (invoiceStatus.status === "paid") {
+    //         console.log("Payment successful!");
+    //       } else {
+    //         console.log("Payment failed or was cancelled.");
+    //       }
+    //     }
+    //   );
+    // } catch (error) {
+    //   console.error("Error opening invoice:", error);
+    // }
+    // const handleClick = () => {
+    //   Construct the deep link URL to send the /pay command to the bot
+    const botLink = `https://t.me/trialTriallairt_bot?start=pay`;
+    window.location.href = botLink;
+    // };
   };
   // const invoice = initInvoice();
   // invoice.open("https://t.me/invoice/abIIks213", "url").then((status) => {
